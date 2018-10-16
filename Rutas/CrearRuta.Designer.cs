c147b87f7c43b23b93854ccddd3b5092216ac426
ncanu <ncanug@miumg.edu.gt>
2018-10-15 22:54:16 -0600

    Cambios de estandarización

--- Rutas/CrearRuta.Designer.cs
+++ Rutas/CrearRuta.Designer.cs
@@ -35,8 +35,28 @@
             this.label9 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.navegador1 = new CapaDiseno.Navegador();
+            this.Lbl_nombre_submodulo = new System.Windows.Forms.Label();
+            this.Txt_numero = new System.Windows.Forms.TextBox();
+            this.Lbl_numero_ruta = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Lbl_vehiculo = new System.Windows.Forms.Label();
+            this.Cbo_vehiculos = new System.Windows.Forms.ComboBox();
+            this.Dtp_fecha = new System.Windows.Forms.DateTimePicker();
+            this.Lbl_fecha = new System.Windows.Forms.Label();
+            this.Txt_envio = new System.Windows.Forms.TextBox();
+            this.Lbl_numero_envio = new System.Windows.Forms.Label();
+            this.Lbl_observaciones = new System.Windows.Forms.Label();
+            this.Txt_observaciones = new System.Windows.Forms.RichTextBox();
+            this.Dgv_rutas = new System.Windows.Forms.DataGridView();
+            this.correlativo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vehiculo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.envio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_rutas)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -55,7 +75,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, -4);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(41, 41);
             this.pictureBox1.TabIndex = 26;
@@ -65,7 +85,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(54, 4);
+            this.label11.Location = new System.Drawing.Point(54, 6);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(97, 20);
             this.label11.TabIndex = 25;
@@ -86,7 +106,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(843, 0);
+            this.button2.Location = new System.Drawing.Point(842, 0);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(41, 41);
             this.button2.TabIndex = 22;
@@ -104,11 +124,183 @@
             this.button1.TabIndex = 21;
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.navegador1.Location = new System.Drawing.Point(7, 117);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(917, 60);
+            this.navegador1.TabIndex = 22;
+            // 
+            // Lbl_nombre_submodulo
+            // 
+            this.Lbl_nombre_submodulo.AutoSize = true;
+            this.Lbl_nombre_submodulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_nombre_submodulo.Location = new System.Drawing.Point(28, 62);
+            this.Lbl_nombre_submodulo.Name = "Lbl_nombre_submodulo";
+            this.Lbl_nombre_submodulo.Size = new System.Drawing.Size(265, 37);
+            this.Lbl_nombre_submodulo.TabIndex = 23;
+            this.Lbl_nombre_submodulo.Text = "Creación de Ruta";
+            // 
+            // Txt_numero
+            // 
+            this.Txt_numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_numero.Location = new System.Drawing.Point(222, 198);
+            this.Txt_numero.Name = "Txt_numero";
+            this.Txt_numero.Size = new System.Drawing.Size(578, 26);
+            this.Txt_numero.TabIndex = 25;
+            // 
+            // Lbl_numero_ruta
+            // 
+            this.Lbl_numero_ruta.AutoSize = true;
+            this.Lbl_numero_ruta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_numero_ruta.Location = new System.Drawing.Point(61, 201);
+            this.Lbl_numero_ruta.Name = "Lbl_numero_ruta";
+            this.Lbl_numero_ruta.Size = new System.Drawing.Size(126, 20);
+            this.Lbl_numero_ruta.TabIndex = 24;
+            this.Lbl_numero_ruta.Text = "Numero de Ruta";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(25, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 1);
+            this.panel1.TabIndex = 26;
+            // 
+            // Lbl_vehiculo
+            // 
+            this.Lbl_vehiculo.AutoSize = true;
+            this.Lbl_vehiculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_vehiculo.Location = new System.Drawing.Point(61, 243);
+            this.Lbl_vehiculo.Name = "Lbl_vehiculo";
+            this.Lbl_vehiculo.Size = new System.Drawing.Size(70, 20);
+            this.Lbl_vehiculo.TabIndex = 27;
+            this.Lbl_vehiculo.Text = "Vehículo";
+            // 
+            // Cbo_vehiculos
+            // 
+            this.Cbo_vehiculos.FormattingEnabled = true;
+            this.Cbo_vehiculos.Location = new System.Drawing.Point(222, 242);
+            this.Cbo_vehiculos.Name = "Cbo_vehiculos";
+            this.Cbo_vehiculos.Size = new System.Drawing.Size(578, 21);
+            this.Cbo_vehiculos.TabIndex = 28;
+            // 
+            // Dtp_fecha
+            // 
+            this.Dtp_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dtp_fecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Dtp_fecha.Location = new System.Drawing.Point(223, 279);
+            this.Dtp_fecha.Name = "Dtp_fecha";
+            this.Dtp_fecha.Size = new System.Drawing.Size(577, 26);
+            this.Dtp_fecha.TabIndex = 30;
+            // 
+            // Lbl_fecha
+            // 
+            this.Lbl_fecha.AutoSize = true;
+            this.Lbl_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_fecha.Location = new System.Drawing.Point(61, 284);
+            this.Lbl_fecha.Name = "Lbl_fecha";
+            this.Lbl_fecha.Size = new System.Drawing.Size(144, 20);
+            this.Lbl_fecha.TabIndex = 29;
+            this.Lbl_fecha.Text = "Fecha de creación:";
+            // 
+            // Txt_envio
+            // 
+            this.Txt_envio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_envio.Location = new System.Drawing.Point(223, 325);
+            this.Txt_envio.Name = "Txt_envio";
+            this.Txt_envio.Size = new System.Drawing.Size(578, 26);
+            this.Txt_envio.TabIndex = 32;
+            // 
+            // Lbl_numero_envio
+            // 
+            this.Lbl_numero_envio.AutoSize = true;
+            this.Lbl_numero_envio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_numero_envio.Location = new System.Drawing.Point(62, 328);
+            this.Lbl_numero_envio.Name = "Lbl_numero_envio";
+            this.Lbl_numero_envio.Size = new System.Drawing.Size(134, 20);
+            this.Lbl_numero_envio.TabIndex = 31;
+            this.Lbl_numero_envio.Text = "Numero de Envío:";
+            // 
+            // Lbl_observaciones
+            // 
+            this.Lbl_observaciones.AutoSize = true;
+            this.Lbl_observaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_observaciones.Location = new System.Drawing.Point(62, 377);
+            this.Lbl_observaciones.Name = "Lbl_observaciones";
+            this.Lbl_observaciones.Size = new System.Drawing.Size(118, 20);
+            this.Lbl_observaciones.TabIndex = 33;
+            this.Lbl_observaciones.Text = "Observaciones:";
+            // 
+            // Txt_observaciones
+            // 
+            this.Txt_observaciones.Location = new System.Drawing.Point(223, 365);
+            this.Txt_observaciones.Name = "Txt_observaciones";
+            this.Txt_observaciones.Size = new System.Drawing.Size(579, 32);
+            this.Txt_observaciones.TabIndex = 34;
+            this.Txt_observaciones.Text = "";
+            // 
+            // Dgv_rutas
+            // 
+            this.Dgv_rutas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Dgv_rutas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_rutas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.correlativo,
+            this.vehiculo,
+            this.fecha,
+            this.envio,
+            this.descripcion});
+            this.Dgv_rutas.Location = new System.Drawing.Point(66, 426);
+            this.Dgv_rutas.Name = "Dgv_rutas";
+            this.Dgv_rutas.Size = new System.Drawing.Size(739, 191);
+            this.Dgv_rutas.TabIndex = 35;
+            // 
+            // correlativo
+            // 
+            this.correlativo.HeaderText = "Correlativo";
+            this.correlativo.Name = "correlativo";
+            // 
+            // vehiculo
+            // 
+            this.vehiculo.HeaderText = "Vehiculo_id";
+            this.vehiculo.Name = "vehiculo";
+            // 
+            // fecha
+            // 
+            this.fecha.HeaderText = "Fecha";
+            this.fecha.Name = "fecha";
+            // 
+            // envio
+            // 
+            this.envio.HeaderText = "Envio_id";
+            this.envio.Name = "envio";
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "Descripcion";
+            this.descripcion.Name = "descripcion";
+            // 
             // CrearRuta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(936, 645);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(168)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(933, 700);
+            this.Controls.Add(this.Dgv_rutas);
+            this.Controls.Add(this.Txt_observaciones);
+            this.Controls.Add(this.Lbl_observaciones);
+            this.Controls.Add(this.Txt_envio);
+            this.Controls.Add(this.Lbl_numero_envio);
+            this.Controls.Add(this.Dtp_fecha);
+            this.Controls.Add(this.Lbl_fecha);
+            this.Controls.Add(this.Cbo_vehiculos);
+            this.Controls.Add(this.Lbl_vehiculo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Txt_numero);
+            this.Controls.Add(this.Lbl_numero_ruta);
+            this.Controls.Add(this.Lbl_nombre_submodulo);
+            this.Controls.Add(this.navegador1);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CrearRuta";
@@ -116,7 +308,9 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_rutas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,5 +322,24 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private CapaDiseno.Navegador navegador1;
+        private System.Windows.Forms.Label Lbl_nombre_submodulo;
+        private System.Windows.Forms.TextBox Txt_numero;
+        private System.Windows.Forms.Label Lbl_numero_ruta;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Lbl_vehiculo;
+        private System.Windows.Forms.ComboBox Cbo_vehiculos;
+        private System.Windows.Forms.DateTimePicker Dtp_fecha;
+        private System.Windows.Forms.Label Lbl_fecha;
+        private System.Windows.Forms.TextBox Txt_envio;
+        private System.Windows.Forms.Label Lbl_numero_envio;
+        private System.Windows.Forms.Label Lbl_observaciones;
+        private System.Windows.Forms.RichTextBox Txt_observaciones;
+        private System.Windows.Forms.DataGridView Dgv_rutas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn correlativo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vehiculo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn envio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
     }
 }
